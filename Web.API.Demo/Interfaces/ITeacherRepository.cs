7b9d3c50b39138828a61546c378135b1910b1243
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-03-18 11:16:28 +0500

    Teacher Repository added

---
+++ Web.API.Demo/Interfaces/ITeacherRepository.cs
@@ -0,0 +1,15 @@
+﻿using Web.API.Demo.Models;
+
+namespace Web.API.Demo.Interfaces
+{
+    public interface ITeacherRepository
+    {
+        ICollection<Teacher> GetAllTeacher();
+        Teacher GetTeacher(int id);
+        Teacher GetByName (string name);
+        ICollection<Teacher> GetByGender(bool isMale);
+        bool IsExist(int id);
+
+
+    }
+}
---
+++ Web.API.Demo/Repositories/TeacherRepository.cs
@@ -0,0 +1,40 @@
+﻿using Web.API.Demo.DbContexts;
+using Web.API.Demo.Interfaces;
+using Web.API.Demo.Models;
+
+namespace Web.API.Demo.Repositories
+{
+    public class TeacherRepository : ITeacherRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public TeacherRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public ICollection<Teacher> GetAllTeacher()
+        {
+            return _appDbContext.Teachers.ToList();
+        }
+
+        public ICollection<Teacher> GetByGender(bool isMale)
+        {
+            return _appDbContext.Teachers.Where(t=>t.IsMale== isMale).ToList();
+        }
+
+        public Teacher GetByName(string name)
+        {
+            return _appDbContext.Teachers.Where(t => t.Name == name).FirstOrDefault();
+        }
+
+        public Teacher GetTeacher(int id)
+        {
+            return _appDbContext.Teachers.Where(t => t.Id == id).FirstOrDefault();
+        }
+
+        public bool IsExist(int id)
+        {
+            return _appDbContext.Teachers.Where(t=>t.Id == id) != null;
+        }
+    }
+}
