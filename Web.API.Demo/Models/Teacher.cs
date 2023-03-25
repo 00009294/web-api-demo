98bf23dbafe71421936b9806f6c02e8941352257
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-03-25 17:25:29 +0500

    Done

--- Web.API.Demo/Models/Teacher.cs
+++ Web.API.Demo/Models/Teacher.cs
@@ -1,16 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Web.API.Demo.Models
 {
     public class Teacher
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        //[Index(IsClustered = true)]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //[TableColumnAttribute(TableColumnType.CreatedAt)]
         public int Id { get; set; } 
         public string Name { get; set; } = String.Empty;
         public bool IsMale { get; set; }
-        public Subject Subject { get; set; } = default!;
+        public Subject Subject { get; set; } 
         public int SubjectId { get; set; }
         public ICollection<StudentTeacher> StudentTeachers { get; set; }
     }
